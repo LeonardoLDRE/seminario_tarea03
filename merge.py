b0cd68883f615d5e5ff1729a81e741e832a41c74
LeonardoLDRE <1559747@senati.pe>
2025-03-20 17:19:29 -0500

    tarea03

--- merge.py
+++ merge.py
@@ -1,11 +1,11 @@
 def merge(l1, l2):
-    r, i, j = [], 0, 0  # Inicializa lista resultado y punteros i, j
-    while i < len(l1) and j < len(l2):  # Mientras haya elementos en ambas listas
-        if l1[i].lower() < l2[j].lower():  # Comparar sin distinguir mayúsculas
-            r.append(l1[i])  # Agregar el menor
-            i += 1  # Avanzar en l1
+    r, i, j = [], 0, 0  
+    while i < len(l1) and j < len(l2):  
+        if l1[i].lower() < l2[j].lower():  
+            r.append(l1[i])  
+            i += 1  
         else:
-            r.append(l2[j])  # Agregar el menor
-            j += 1  # Avanzar en l2
-    return r + l1[i:] + l2[j:]  # Agregar elementos restantes
+            r.append(l2[j])  
+            j += 1 
+    return r + l1[i:] + l2[j:]  
 print(merge(["ADAPTADOR", "USB", "TECLADO"], ["MEMORIA", "MOUSE", "IMPRESORA"]))
